2780d08ff214714f5e5b386cea901de384c6aaf1
bbou <bbou@ac-toulouse.fr>
2017-10-01 11:25:22 +0200

    commonlib check for theme existence

--- commonLib/src/main/java/org/treebolic/AppCompatCommonUtils.java
+++ commonLib/src/main/java/org/treebolic/AppCompatCommonUtils.java
@@ -3,11 +3,14 @@ package org.treebolic;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.preference.PreferenceManager;
-
+import android.util.Log;
 
 public class AppCompatCommonUtils
 {
+	static private final String TAG = "AppCompatCommonUtils";
+
 	static public final String PREF_THEME = "pref_theme";
 
 	/**
@@ -36,6 +39,35 @@ public class AppCompatCommonUtils
 	{
 		final SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 		final String value = sharedPrefs.getString(PREF_THEME, null);
-		return value == null ? null : Integer.parseInt(value);
+		if (value == null)
+		{
+			return null;
+		}
+		int intValue = Integer.parseInt(value);
+		if (intValue == 0)
+		{
+			final SharedPreferences.Editor editor = sharedPrefs.edit();
+			editor.remove(PREF_THEME);
+			editor.apply();
+			return null;
+		}
+
+		final Resources resources = context.getResources();
+		try
+		{
+			final String type = resources.getResourceTypeName(intValue);
+			if ("style".equals(type))
+			{
+				Log.d(TAG, "Theme " + type + ' ' + resources.getResourceName(intValue) + ' ' + context.getResources().getResourceEntryName(intValue));
+				return intValue;
+			}
+		}
+		catch (Resources.NotFoundException e)
+		{
+			final SharedPreferences.Editor editor = sharedPrefs.edit();
+			editor.remove(PREF_THEME);
+			editor.apply();
+		}
+		return null;
 	}
 }
