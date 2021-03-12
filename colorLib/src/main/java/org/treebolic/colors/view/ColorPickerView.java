091ea484264b9c90efcec3783c978fd7cbc4b4a7
1313ou <1313ou@gmail.com>
2021-03-12 18:04:45 +0100

    lint

--- colorLib/src/main/java/org/treebolic/colors/view/ColorPickerView.java
+++ colorLib/src/main/java/org/treebolic/colors/view/ColorPickerView.java
@@ -911,9 +911,7 @@ public class ColorPickerView extends View
 
 				finalWidth = widthAllowed;
 			}
-			else
-				//noinspection ConstantConditions
-				if (heightMode == MeasureSpec.EXACTLY && widthMode != MeasureSpec.EXACTLY)
+			else if (/*heightMode == MeasureSpec.EXACTLY && */ widthMode != MeasureSpec.EXACTLY)
 				{
 					// The height has been specified exactly, we need to stay within this height and adopt the width.
 
