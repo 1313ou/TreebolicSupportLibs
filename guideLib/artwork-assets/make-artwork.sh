9cab4db332886b573828a9d488a86ef64390e1a6
1313ou <1313ou@gmail.com>
2023-01-08 11:39:36 +0100

    credits help on moves

--- guideLib/artwork-assets/make-artwork.sh
+++ guideLib/artwork-assets/make-artwork.sh
@@ -3,8 +3,10 @@
 source "../../../make-artwork-lib.sh"
 
 help="menu*.svg search*.svg tip*.svg toolbar*.svg"
+move="moves.svg"
 splash="splash*.svg"
 
 make_assets "${help}" 48
+make_assets "${move}" 96
 make_assets "${splash}" 150
 
