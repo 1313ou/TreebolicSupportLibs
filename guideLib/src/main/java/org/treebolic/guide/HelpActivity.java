9a9651adb441dac8b3b307c90c86a56d6f207f29
1313ou <1313ou@gmail.com>
2024-07-09 17:35:42 +0200

    redundant suppression

--- guideLib/src/main/java/org/treebolic/guide/HelpActivity.java
+++ guideLib/src/main/java/org/treebolic/guide/HelpActivity.java
@@ -75,7 +75,6 @@ public class HelpActivity extends AppCompatCommonActivity
 		//webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 		webView.setWebViewClient(new WebViewClient()
 		{
-			@SuppressWarnings("deprecation")
 			@Override
 			public void onReceivedError(final WebView view, final int errorCode, final String description, final String failingUrl)
 			{
@@ -90,7 +89,6 @@ public class HelpActivity extends AppCompatCommonActivity
 				Log.e(TAG, error.getDescription().toString() + ',' + error.getErrorCode());
 			}
 
-			@SuppressWarnings("deprecation")
 			@Override
 			public boolean shouldOverrideUrlLoading(@NonNull final WebView view, final String url)
 			{
