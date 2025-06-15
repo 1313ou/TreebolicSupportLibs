96292be86331979c37198ac48e3391d51f17113c
1313ou <1313ou@gmail.com>
2025-06-15 11:18:04 +0200

    migration to kts

---
+++ downloadLib/build.gradle.kts
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2019. Bernard Bou <1313ou@gmail.com>
+ */
+
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+private val vCompileSdk by lazy { rootProject.extra["compileSdk"] as Int }
+private val vMinSdk by lazy { rootProject.extra["minSdk"] as Int }
+
+android {
+
+    namespace = "org.treebolic.download"
+
+    compileSdk = vCompileSdk
+
+    defaultConfig {
+        minSdk = vMinSdk
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":commonLib"))
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.compress)
+
+    implementation(libs.core.ktx)
+    coreLibraryDesugaring(libs.desugar)
+}
---
+++ searchLib/build.gradle.kts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2019. Bernard Bou <1313ou@gmail.com>
+ */
+
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+private val vCompileSdk by lazy { rootProject.extra["compileSdk"] as Int }
+private val vMinSdk by lazy { rootProject.extra["minSdk"] as Int }
+
+android {
+
+    namespace = "org.treebolic.search"
+
+    compileSdk = vCompileSdk
+
+    defaultConfig {
+        minSdk = vMinSdk
+        multiDexEnabled = true
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":wheelLib"))
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("com.google.android.material:material:1.12.0")
+
+    implementation(libs.core.ktx)
+    coreLibraryDesugaring(libs.desugar)
+}
