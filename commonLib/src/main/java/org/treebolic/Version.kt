7a78a8fe797d61556f98295e32374e4692eb751b
1313ou <1313ou@gmail.com>
2026-04-30 21:20:26 +0200

    move

---
+++ commonLib/src/main/java/org/treebolic/Version.kt
@@ -0,0 +1,62 @@
+package org.treebolic
+
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Build
+import android.text.SpannableStringBuilder
+import org.treebolic.common.BuildConfig
+
+object Version {
+    /**
+     * Build time
+     *
+     * @return build time
+     */
+    fun buildTime(): String {
+        return BuildConfig.BUILD_TIME
+    }
+
+    /**
+     * Git hash
+     *
+     * @return git hasj
+     */
+    fun gitHash(): String {
+        return BuildConfig.GIT_HASH
+    }
+
+    fun appVersion(context: Context): CharSequence {
+        val sb = SpannableStringBuilder()
+        sb.apply {
+            val packageName = context.applicationInfo.packageName
+            append(packageName)
+            append('\n')
+            val pInfo: PackageInfo
+            try {
+                pInfo = context.packageManager.getPackageInfo(packageName, 0)
+                val code = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) pInfo.longVersionCode else @Suppress("DEPRECATION") pInfo.versionCode.toLong()
+                append("version: ")
+                append(code.toString())
+                append('\n')
+            } catch (e: PackageManager.NameNotFoundException) {
+                append("package info: ")
+                append(e.message)
+                append('\n')
+            }
+            append("api: ")
+            append(Build.VERSION.SDK_INT.toString())
+            append(' ')
+            append(Build.VERSION.CODENAME)
+            append('\n')
+
+            append("build time: ")
+            append(buildTime())
+            append('\n')
+            append("git commit hash: ")
+            append(gitHash())
+            append('\n')
+        }
+        return sb
+    }
+}
